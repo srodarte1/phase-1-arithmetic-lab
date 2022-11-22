8de658c46ddc2833f404bf5f96902548d9cad401
+ <rodarte_sarah@yahoo.com>
2022-11-21 21:54:55 -0700

    commit tree 6209c5ef1f1641d5f40607c040a322acd79f45d3

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 18
+let num4 = 7
+const mod = num3 % num4
+
+const max = Math.ceil(20)
+
